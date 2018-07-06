f610afc3222913189bd46b4c402531712b3c1739
Easiv <easiv89@gmail.com>
2018-07-06 15:14:05 +0200

    torii basic login implementation

---
+++ app/routes/application.js
@@ -0,0 +1,17 @@
+import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
+export default Route.extend({
+  session: inject(),
+  beforeModel: function() {
+    return this.get('session').fetch().catch(function() {});
+  },
+  actions: {
+    signIn: function() {
+      this.get('session').open('firebase', { provider: 'google'}).then(function() { // function(data)
+      });
+    },
+    signOut: function() {
+      this.get('session').close();
+    }
+  }
+});
