33139af7bf4a42423eab2f379d39c31c4b092b29
Easiv <easiv89@gmail.com>
2018-07-19 12:18:44 +0200

    more generic and overall better highlight acceptance test

--- tests/acceptance/page/new-test.js
+++ tests/acceptance/page/new-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { fillIn, click, visit, find } from '@ember/test-helpers';
+import { fillIn, click, visit, find, andThen } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | page/new', function(hooks) {
@@ -8,9 +8,11 @@ module('Acceptance | page/new', function(hooks) {
   test('is it really highlighting tho', async function(assert) {
 
     await visit('/page/new');
-    await fillIn('input#highlightInput', 'com');
+    let highlightInput = document.querySelectorAll('.flexSpace input')[2];
+    await fillIn('#textArea', 'fancy test text');
+    await fillIn(highlightInput, 'es');
     await click('button#highbutton');
 
-    assert.equal(find('#textArea').innerHTML, '<mark data-markjs="true">com</mark>plex');
+    assert.equal(find('#textArea').innerHTML, 'fancy t<mark data-markjs="true">es</mark>t text');
   });
 });
