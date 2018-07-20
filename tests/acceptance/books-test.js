70d06ac3578aa3e46666c3671c6aba53df58cbc6
Easiv <easiv89@gmail.com>
2018-07-20 15:53:42 +0200

    Tests for adding books

--- tests/acceptance/books-test.js
+++ tests/acceptance/books-test.js
@@ -1,14 +1,13 @@
 import { module, test } from 'qunit';
-import { visit, click, findAll, currentURL } from '@ember/test-helpers';
+import { visit, click, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | books', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('are books displaying', async function(assert) {
+  test('visiting new book page', async function(assert) {
     await visit('/books');
     await click('[data-test-create-book-button]');
-    await findAll('model.book');
 
     assert.equal(currentURL(), '/books/new');
   });
