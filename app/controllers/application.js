5ac4e554e2f407cb284498122deef2f0117dae49
Easiv <easiv89@gmail.com>
2018-07-24 12:55:44 +0200

    Changed login to auth0, added transition in book creation.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,30 +4,21 @@ import { inject } from '@ember/service';
 export default Controller.extend({
   session: inject(),
 
-  // async _createUser({ uid, currentUser: { displayName: name } }) {
-  //   let userFound = false;
-  //   await this.filterEqual(this.store, 'user', { uid }, function() {
-  //     userFound = true;
-  //   });
-  //   if (userFound) {
-  //     return;
-  //   }
-  //   let user = this.store.createRecord('user', {
-  //     name,
-  //     uid
-  //   }).save();
-  //   this.set('session.currentUser', { id: '5' });
-  // },
-
   actions: {
-    signIn() {
-      this.get('session').open('firebase', { provider: 'google' }).then(() => {
-      });
-
+    login() {
+      const lockOptions = {
+       auth: {
+         params: {
+           scope: 'openid user_metadata'
+         }
+       }
+      };
+      this.get('session').authenticate('authenticator:auth0-lock', lockOptions);
     },
 
-    signOut() {
-      this.get('session').close();
+    logout() {
+      this.get('session').invalidate();
     }
   }
+
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,4 @@
 import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
 export default Route.extend({
-  session: inject(),
-  beforeModel() {
-    return this.get('session').fetch().catch(function() {});
-  }
 });
