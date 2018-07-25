d5cd0e97a8352f7afce94fa77dd677e0e0f9c942
Easiv <easiv89@gmail.com>
2018-07-25 14:50:06 +0200

    CI fixes

--- app/authenticators/auth0-lock-firebase.js
+++ app/authenticators/auth0-lock-firebase.js
@@ -1,6 +1,8 @@
 import Authenticator from 'ember-simple-auth-auth0/authenticators/auth0-lock';
+import getSessionExpiration from '../utils/get-session-expiration';
 import { inject as service } from '@ember/service';
-
+import now from '../utils/now';
+import RSVP from 'rsvp';
 
 export default Authenticator.extend({
   store: service(),
@@ -8,30 +10,30 @@ export default Authenticator.extend({
 
   authenticate(options) {
     return this.get('auth0').showLock(options).then((sessionData) => {
-      this.set('session.user', this._findOrCreateUser(sessionData.profile))
+      this.set('session.user', this._findOrCreateUser(sessionData.profile));
     });
   },
 
   restore(data) {
-    this.set('session.user', this._findOrCreateUser(data.profile))
+    this.set('session.user', this._findOrCreateUser(data.profile));
 
-    const expiresAt = getSessionExpiration(data || {});
-    if(expiresAt > now()) {
+    let expiresAt = getSessionExpiration(data || {});
+    if (expiresAt > now()) {
       return RSVP.resolve(data);
     } else {
       return RSVP.reject();
     }
   },
 
-  _findOrCreateUser({email, picture: photoUrl, name}) {
+  _findOrCreateUser({ email, picture: photoUrl, name }) {
     return this.get('store').query('user', { email })
       .then((users) =>
-        users.get('firstObject') || this._createUser(photoUrl, name, email)
+        users.get('firstObject') || this._createUser(photoUrl, name, email);
       );
   },
 
   _createUser(photoUrl, name, email) {
-    let user = this.get('store').createRecord('user', {photoUrl, name, email});
+    let user = this.get('store').createRecord('user', { photoUrl, name, email });
     return user.save();
   }
 })
