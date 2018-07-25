218b3a5ef171e223f826301506aababc97fe2616
Easiv <easiv89@gmail.com>
2018-07-25 12:25:03 +0200

    Improved routing, pages editor now displaying in book view.

---
+++ app/controllers/books/book/pages/new.js
@@ -0,0 +1,22 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service'
+
+export default Controller.extend({
+
+  session: service(),
+  routing: service('-routing'),
+
+  actions: {
+    createPage() {
+
+      let newPage = this.store.createRecord('page', {
+        number: this.get('number'),
+        content: this.get('content'),
+        book: this.get('book')
+      });
+      newPage.save().then(() => {
+        this.get('routing').transitionTo('books.pages', []);
+      });
+    }
+  }
+});
