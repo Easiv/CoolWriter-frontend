0155de70733f6b784acce7ea99c886a6e8e6fcab
Easiv <easiv89@gmail.com>
2018-08-06 12:50:26 +0200

    All tests green and fancy-looking

--- tests/acceptance/books/book/pages-test.js
+++ tests/acceptance/books/book/pages-test.js
@@ -1,78 +1,55 @@
 import { module, test } from 'qunit';
-import { find } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
-import { authenticateSession } from 'ember-simple-auth/test-support';
 import page from 'coolwriter/tests/pages/books/book/pages';
 
 module('Acceptance | books/pages', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('is it really highlighting though', async function(assert) {
+  test('Is it really highlighting though?', async function(assert) {
 
-    server.create('book', 1);
-    let textArea = '[data-test-text-area]';
-    let isMarked = 'fancy t<mark data-markjs="true">es</mark>t text';
-
-    await authenticateSession({
-      profile: { email: 'mirage@fake.do' }
-    });
-
+    await page.createBook();
+    await page.logUser();
     await page.visitBooks();
     await page.clickWrite();
     await page.fillTextArea('fancy test text');
     await page.fillMarkInput('es');
     await page.clickHighlight();
 
-    assert.equal(find(textArea).innerHTML, isMarked);
+    assert.equal(page.areaContent(), page.isMarked);
   });
 
-  test('does it change the font size', async function(assert) {
+  test('Does it change the font size?', async function(assert) {
 
-    server.createList('book', 1);
-    let textArea = '[data-test-text-area]';
-
-    await authenticateSession({
-      profile: { email: 'mirage@fake.do' }
-    });
-
+    await page.createBook();
+    await page.logUser();
     await page.visitBooks();
     await page.clickWrite();
     await page.fillSizeInput(25);
 
-    assert.equal(find(textArea).style.fontSize, '25px');
+    assert.equal(page.areaFontSize(), '25px');
   });
 
-  test('does it change the font family', async function(assert) {
+  test('Does it change the font family?', async function(assert) {
 
-    server.createList('book', 1);
-    let textArea = '[data-test-text-area]';
-
-    await authenticateSession({
-      profile: { email: 'mirage@fake.do' }
-    });
-
+    await page.createBook();
+    await page.logUser();
     await page.visitBooks();
     await page.clickWrite();
     await page.fillFamilyInput('arial');
 
-    assert.equal(find(textArea).style.fontFamily, 'arial');
+    assert.equal(page.areaFontFamily(), 'arial');
   });
 
-  test('is it really clearing the text', async function(assert) {
+  test('Is it really clearing the text?', async function(assert) {
 
-    server.createList('book', 1);
-    let textArea = '[data-test-text-area]';
-
-    await authenticateSession({
-      profile: { email: 'mirage@fake.do' }
-    });
-
+    await page.createBook();
+    await page.logUser();
     await page.visitBooks();
     await page.clickWrite();
     await page.clickClear();
 
-    assert.equal(find(textArea).innerHTML, '');
+    assert.equal(page.areaContent(), '');
   });
 });
--- tests/pages/books/book/pages.js
+++ tests/pages/books/book/pages.js
@@ -1,13 +1,43 @@
 import { create, visitable, clickable, fillable } from 'ember-cli-page-object';
+import { authenticateSession } from 'ember-simple-auth/test-support';
+import { find } from '@ember/test-helpers';
 
 export default create({
+
+  createBook() {
+    server.create('book', 1);
+  },
+
+  logUser() {
+    return authenticateSession({
+      profile: { email: 'mirage@fake.do' }
+    });
+  },
+
+  areaContent() {
+    return find(this.textArea).innerHTML;
+  },
+
+  areaFontSize() {
+    return find(this.textArea).style.fontSize;
+  },
+
+  areaFontFamily() {
+    return find(this.textArea).style.fontFamily;
+  },
+
+  textArea: '[data-test-text-area]',
+  isMarked: 'fancy t<mark data-markjs="true">es</mark>t text',
+
   visitBooks: visitable('/books'),
+
   clickWrite: clickable('[data-test-write-button]'),
+  clickHighlight: clickable('[data-test-highlight-button]'),
+  clickClear: clickable('[data-test-clear-button]'),
+
   fillTextArea: fillable('[data-test-text-area]'),
   fillMarkInput: fillable('[data-test-highlight-input] input'),
   fillSizeInput: fillable('[data-test-size-input] input'),
-  fillFamilyInput: fillable('[data-test-family-input] input'),
-  clickHighlight: clickable('[data-test-highlight-button]'),
-  clickClear: clickable('[data-test-clear-button]')
+  fillFamilyInput: fillable('[data-test-family-input] input')
 
 });
