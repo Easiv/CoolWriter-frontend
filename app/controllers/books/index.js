d9d725d8eb4f279dd9bf9cb604dcf3a23fd16b65
Easiv <easiv89@gmail.com>
2018-08-17 15:48:34 +0200

    Now u can search the books by the title

--- app/controllers/books/index.js
+++ app/controllers/books/index.js
@@ -7,6 +7,20 @@ export default Controller.extend({
   i18n: service(),
   session: service(),
 
+  queryParams: ['title'],
+  title: null,
+
+  filterBooks: computed('title', function(){
+    let title = this.get('title');
+    if(title) {
+      let books = this.store.query('book', {
+        orderBy: 'title',
+        equalTo: title
+      });
+      return books;
+    }
+  }),
+
   init() {
     this._super(...arguments);
 
