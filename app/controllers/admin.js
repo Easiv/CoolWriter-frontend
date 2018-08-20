99e906d168f9e094ae65c60f973b29075a90ed12
Easiv <easiv89@gmail.com>
2018-08-20 13:13:11 +0200

    Autosearching while filling input, empty input returns list of all books

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,11 +1,12 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import { computed, observer } from '@ember/object';
 
 export default Controller.extend({
   queryParams: ['title'],
   title: null,
+  search: null,
 
-  filterBooks: computed('title', function(){
+  filterBooks: computed('title', function() {
     let title = this.get('title');
     if(title) {
       let books = this.store.query('book', {
@@ -15,4 +16,9 @@ export default Controller.extend({
       return books;
     }
   }),
+
+  triggerSearch: observer('search', function() {
+    let title = this.get('search');
+    this.transitionToRoute({ queryParams: { title } });
+  })
 });
